3334cfe2b742e4ad6b9202cb69d76573f51b8739
Yasin <hiraniyasin162000@gmail.com>
2021-03-04 12:15:15 +0530

    background changed and search button added

--- src/App.js
+++ src/App.js
@@ -3,30 +3,37 @@ import './App.css';
 
 function App() {
   const [city, setCity] = useState(null);
-  const [search, setSearch] = useState("Mumbai");
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState('Mumbai')
   useEffect( () => {
     const fetchApi =async () =>{
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=074da6b6822590cafa8ebc99142b2fcb`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=074da6b6822590cafa8ebc99142b2fcb`;
     const response = await fetch(url);
     const resJson = await response.json();
     setCity(resJson.main);
     }
     fetchApi();
-  },[search])
+  },[query])
   return (
     <>
     <div className="container">
       <div className="box">
         <div className="inputdata">
+          <form onSubmit={(e) => {
+              e.preventDefault();
+              setQuery(search);
+          }}>
           <input type="search" placeholder="enter city name" value={search} className="inputfield" onChange={(event) => {
               setSearch(event.target.value);
           }}/>
+          <button className="btn-get-weather" type="submit"></button>
+          </form>
         </div>
         {!city ? (
           <p className="errormsg">No city found</p>
         ) : (
             <div className="info">
-            <h2 className="location"><i className="fa fa-street-view" aria-hidden="true"></i>{search}</h2>
+            <h2 className="location"><i className="fa fa-street-view" aria-hidden="true"></i>{query}</h2>
             <h1 className="temp">{city.temp}<sup>o</sup>Cel</h1>
             <h3 className="minmax">Min:{city.temp_min}<sup>o</sup>cel | Max:{city.temp_max}<sup>o</sup>cel</h3>
           </div>
